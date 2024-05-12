159762a2ba86c6791ff3aab07b373f3947e586ad
Devanshu <devanshupandey281@gmail.com>
2024-05-13 00:33:43 +0530

    project complete

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ApiService } from '../services/api.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,33 +9,63 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  name = '';
   title = 'pagination';
   page: number = 1;
   count: number = 0;
   tableSize: number = 10;
   tableSizes: any = [10, 25, 50, 75, 100];
+  isLoading: boolean = false;
+  users: any[] = [];
+  repos: any = [];
+  reposLanguages: any = [];
+  allLanguages: any[] = [];
 
-  name = '';
-  public users: any[] = [];
-  public repos: any = [];
   constructor(private username: ApiService) {}
 
   errMessage = '';
   searchByName(searchkeyword: string) {
-    this.name = searchkeyword;
-    this.username.getUser(this.name).subscribe(
-      (data) => {
-        this.users = [data];
+    this.isLoading = true;
+    this.name = searchkeyword.trim();
+    if (this.name != '') {
+      this.username.getUser(this.name).subscribe(
+        (data) => {
+          this.users = [data];
+        },
+        (error) => {
+          this.errMessage = error;
+          console.log(error);
+        }
+      );
+      this.username.getRepos(this.name).subscribe((repo) => {
+        this.repos = repo;
+        this.allLanguages = [];
+        this.fetchLanguagesForRepos();
+      });
+    } else {
+      console.log('enter a name before search');
+    }
+  }
+
+  fetchLanguagesForRepos() {
+    const requests = this.repos.map((repo: any) => {
+      return this.username.fetchLanguages(repo.languages_url);
+    });
+
+    forkJoin(requests).subscribe(
+      (responses: any) => {
+        responses.forEach((data: any) => {
+          const languages = Object.keys(data);
+          this.reposLanguages = languages;
+          this.allLanguages.push(this.reposLanguages);
+        });
+        this.isLoading = false;
       },
       (error) => {
-        this.errMessage = error;
-        console.log(error);
+        console.error('Error fetching languages:', error);
+        this.isLoading = false;
       }
     );
-    this.username.getRepos(this.name).subscribe((repo) => {
-      this.repos = repo;
-      console.log(this.repos);
-    });
   }
 
   clearInputField(input: NgForm) {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -8,31 +12,60 @@ import { catchError } from 'rxjs/operators';
 })
 export class ApiService {
   constructor(private httpClient: HttpClient) {}
+  private authToken = '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';
+
+  getUser(githubUsername: string) {
+    return this.httpClient
+      .get(`https://api.github.com/users/${githubUsername}`, {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `token ${this.authToken}`
+        ),
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
+
+  getRepos(githubUsername: string) {
+    return this.httpClient
+      .get(`https://api.github.com/users/${githubUsername}/repos`, {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `token ${this.authToken}`
+        ),
+      })
+      .pipe(catchError(this.handleError));
+  }
+  fetchLanguages(repoLanguagesUrl: string) {
+    return this.httpClient
+      .get(repoLanguagesUrl, {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `token ${this.authToken}`
+        ),
+      })
+      .pipe(catchError(this.handleError));
+  }
 
   private handleError(error: HttpErrorResponse) {
     let message = '';
     if (error.error instanceof ErrorEvent) {
       console.error('An error occurred:', error.error);
     } else {
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-      message = `Please check your UserName. Something bad happened; Backend returned code ${error.status}...`;
+      if (error.status == 403) {
+        message =
+          'You exceed the limit for search | Wait for an hour then try again';
+        console.log(
+          'GitHub allow only 60 request in an Hour for unauthorized Users'
+        );
+      } else {
+        console.error(
+          `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+        );
+        message = `Please check your UserName. Something bad happened; Backend returned code ${error.status}...`;
+      }
     }
     return throwError(() => new Error(message));
   }
-
-  getUser(githubUsername: string) {
-    return this.httpClient
-      .get(`https://api.github.com/users/${githubUsername}`)
-      .pipe(catchError(this.handleError));
-  }
-
-  getRepos(githubUsername: string) {
-    return this.httpClient.get(
-      `https://api.github.com/users/${githubUsername}/repos`
-    );
-  }
-
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
 }
